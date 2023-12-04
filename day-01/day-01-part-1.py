8c4c70036b9b0bd49df8bfa5f2d9ef588a11f97b
Jon Schneider <jon.schneider@appfolio.com>
2023-12-03 19:12:16 -0500

    Refactor into functions

--- day-01/day-01-part-1.py
+++ day-01/day-01-part-1.py
@@ -1,20 +1,24 @@
-with open('input.txt') as file:
-    lines = [line.rstrip() for line in file]
+def read_input_file_lines():
+    with open('input.txt') as file:
+        lines = [line.rstrip() for line in file]
+    return lines
 
-print(lines)
+def calibration_total(lines):
+    total = 0
 
-calibration_total = 0
+    for line in lines:
+        for character in line:
+            if character.isdigit():
+                first_digit_character = character
+                break
+        for character in reversed(line):
+            if character.isdigit():
+                last_digit_character = character
+                break
+        calibration_value_string = first_digit_character + last_digit_character
+        total += int(calibration_value_string)
 
-for line in lines:
-    for character in line:
-        if character.isdigit():
-            first_digit_character = character
-            break
-    for character in reversed(line):
-        if character.isdigit():
-            last_digit_character = character
-            break
-    calibration_value_string = first_digit_character + last_digit_character
-    calibration_total += int(calibration_value_string)
+    return total
 
-print(calibration_total)
+lines = read_input_file_lines()
+print(calibration_total(lines))
