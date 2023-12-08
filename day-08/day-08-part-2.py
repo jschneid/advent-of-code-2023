4e6edeb66f73ee019e745f05836d11519c9052af
Jon Schneider <jon.schneider@appfolio.com>
2023-12-08 09:56:24 -0500

    Day 8 part 2

--- day-08/day-08-part-2.py
+++ day-08/day-08-part-2.py
@@ -1,3 +1,5 @@
+from math import lcm
+
 class NodePair:
     def __init__(self, left, right):
         self.left = left
@@ -26,15 +28,48 @@ def get_start_nodes(network):
 def steps_to_reach_nodes_ending_in_Z(nodes, instructions, network):
     instruction_pointer = 0
     steps = 0
-    while not are_all_nodes_destinations(nodes):
-        steps += 1
+
+    # For my input, the count of steps needed for each "thread" to find a solution 
+    # exactly equals the cycle time (in steps) to repeatedly circle back around to 
+    # that same solution node.
+    #
+    # Also (in my input), the path from each start node only ever ends up passing 
+    # through one distinct solution node (and no other solution nodes).
+    #
+    # (I made these observations by running the program for just one of each of the
+    # start nodes at a time; and noting the node name and step count for each time
+    # a destination node was reached.)
+
+    solution_steps = []
+
+    thread_count = len(nodes)
+    while len(solution_steps) < thread_count:
+        # If any "thread" has reached a destination, record the number of steps that
+        # was taken to get there.
+        for node in nodes:
+            if (is_node_destination(node)):
+                solution_steps.append(steps)
+
+        # Save a bit of processing time by stopping processing for a given "thread" when 
+        # that thread has found a destination. (This is feasible due to the observation that
+        # (in my input) each thread only ever reaches a single distinct destination node.)
+        nodes = [node for node in nodes if not is_node_destination(node)]
+
+        # Advance each "thread" to the next node in its path.
         for node_index in range(len(nodes)):
             nodes[node_index] = next_node(instructions[instruction_pointer], nodes[node_index], network)
-        instruction_pointer = increment_instruction_pointer(instruction_pointer, instructions)
-    return steps
 
-def are_all_nodes_destinations(nodes):
-    return all(node[2] == 'Z' for node in nodes)
+        instruction_pointer = increment_instruction_pointer(instruction_pointer, instructions)
+        steps += 1
+
+    # Now that we know the count of steps for each start node that were needed to reach 
+    # a destination node, plus the observations noted above: We can calculate the time needed 
+    # for all of the threads to converge on their destination at the same time by finding 
+    # the least common multiple of each of those values.
+    return lcm(*solution_steps)
+
+def is_node_destination(node):
+    return node[2] == 'Z'
 
 def next_node(instruction, node, network):
     if instruction == 'L':
