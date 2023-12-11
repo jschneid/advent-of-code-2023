9bc28b7ba2af6ea32bcf629c45e3ceb70fe717e3
Jon Schneider <jon.schneider@appfolio.com>
2023-12-11 08:30:05 -0500

    Day 11 part 1

---
+++ day-11/day-11-part-1.py
@@ -0,0 +1,56 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __lt__(self, other):
+        return self.y < other.y or (self.y == other.y and self.x < other.x)
+    
+def read_input_file_lines():
+    with open('input.txt') as file:
+        lines = [line.rstrip() for line in file]
+    return lines
+
+def galaxies_from_map(map):
+    galaxies = []
+    for y, line in enumerate(map):
+        for x, character in enumerate(line):
+            if character == '#':
+                galaxies.append(Point(x, y))
+    return galaxies
+
+def expand_universe_horizontally(galaxies, x):
+    for galaxy in galaxies:
+        if galaxy.x > x:
+            galaxy.x += 1
+
+def expand_universe_vertically(galaxies, y):
+    for galaxy in galaxies:
+        if galaxy.y > y:
+            galaxy.y += 1
+
+def expand_the_universe(galaxies, max_x, max_y):
+    for x in range(max_x, -1, -1):
+        if all(galaxy.x != x for galaxy in galaxies):
+            expand_universe_horizontally(galaxies, x)
+    for y in range(max_y, -1, -1):
+        if all(galaxy.y != y for galaxy in galaxies):
+            expand_universe_vertically(galaxies, y)
+        
+def galaxy_pairs(galaxies):
+    for galaxy in galaxies:
+        for other_galaxy in galaxies:
+            if galaxy < other_galaxy:
+                yield (galaxy, other_galaxy)
+
+def manhattan_distance(point1, point2):
+    return abs(point1.x - point2.x) + abs(point1.y - point2.y)
+
+def sum_of_manhattan_distances(galaxy_pairs):
+    return sum(manhattan_distance(galaxy_pair[0], galaxy_pair[1]) for galaxy_pair in galaxy_pairs)
+
+map = read_input_file_lines()
+galaxies = galaxies_from_map(map)
+expand_the_universe(galaxies, len(map[0]), len(map))
+galaxy_pairs = galaxy_pairs(galaxies)
+print(sum_of_manhattan_distances(galaxy_pairs))
