a73a40b997b0d4fa59e58a30663d3c1269a0a5c5
Jon Schneider <jon.schneider@appfolio.com>
2023-12-17 17:06:32 -0500

    Cut runtime from ~20 minutes to ~6

--- day-17/day-17-part-1.py
+++ day-17/day-17-part-1.py
@@ -1,3 +1,5 @@
+import time
+
 class State:
     def __init__(self, y, x, last_move_direction, last_move_streak, accumulated_heat_loss):
         self.y = y
@@ -6,11 +8,14 @@ class State:
         self.last_move_streak = last_move_streak
         self.accumulated_heat_loss = accumulated_heat_loss
 
-    def __str__(self) -> str:
-        return f'(y={self.y}, x={self.x}, last_move_direction={self.last_move_direction}, last_move_streak={self.last_move_streak}, accumulated_heat_loss={self.accumulated_heat_loss})'
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.last_move_direction == other.last_move_direction and self.last_move_streak == other.last_move_streak
 
-    def __repr__(self) -> str:
-        return self.__str__()
+    def __hash__(self):
+        return self.x + 200 * self.y + 30000 * hash(self.last_move_direction) + 10000000 * self.last_move_streak
+    
+    def __lt__(self, other):
+        return self.accumulated_heat_loss < other.accumulated_heat_loss
 
 def read_input_file_lines():
     with open('input.txt') as file:
@@ -42,40 +47,47 @@ def possible_next_moves(map, previous_state, all_states):
     return eligible_next_moves
 
 def state_already_evaluated(move, all_states):
-    # TODO If this is slow optimize the datatype to have it be faster than O(N)
+    if move in all_states:
+        return True
+    
+    extra_move = State(move.y, move.x, move.last_move_direction, move.last_move_streak + 1, 0)
+    if extra_move in all_states:
+        return True
+
+    extra_extra_move = State(move.y, move.x, move.last_move_direction, move.last_move_streak + 2, 0)
+    if extra_extra_move in all_states:
+        return True
+
+    # Now that we've done the fast O(1) checks that we're able to do, do the slow O(N) check
     for state in all_states:
-        # TODO maybe also throw out moves with greater heat when streak is the same?
         if state.y == move.y and state.x == move.x and state.last_move_direction == move.last_move_direction and state.last_move_streak <= move.last_move_streak and state.accumulated_heat_loss <= move.accumulated_heat_loss:
             return True
+
     return False
 
 def minimum_heat_loss(map):
     initial_state = State(0, 0, 'south', 0, 0)
-    max_solution_heat_loss = 1400
     states_to_evaluate = possible_next_moves(map, initial_state, [])
     
-    all_states = []
+    all_states = set()
     for state in states_to_evaluate:
-        all_states.append(state)
+        all_states.add(state)
 
-    while len(states_to_evaluate) > 0:
+    while True:
         # Sort states_to_evaluate by accumulated_heat_loss, lowest first
-        states_to_evaluate.sort(key=lambda x: x.accumulated_heat_loss)
+        states_to_evaluate = sorted(states_to_evaluate)
 
         state = states_to_evaluate.pop(0)
 
-        if state.accumulated_heat_loss >= max_solution_heat_loss:
-            continue
-
         if state.y == len(map) - 1 and state.x == len(map[0]) - 1:
-            max_solution_heat_loss = state.accumulated_heat_loss
-            print("New minimum! " + str(max_solution_heat_loss))
-        else:
-            new_states = possible_next_moves(map, state, all_states)
-            states_to_evaluate += new_states
-            all_states += new_states
+            return state.accumulated_heat_loss
 
-    return max_solution_heat_loss    
+        new_states = possible_next_moves(map, state, all_states)
+        states_to_evaluate += new_states
+        for new_state in new_states:
+            all_states.add(new_state)
 
+start_time = time.time()
 map = read_input_file_lines()
 print(minimum_heat_loss(map))
+print("--- %s seconds ---" % (time.time() - start_time))
