13372a7387d6c48bb9c2395ba6abc9e11f2baa7a
Jon Schneider <jon.schneider@appfolio.com>
2023-12-19 16:03:22 -0500

    Work in progress - Runtime still too long
    - Compressed the workflow state diagram (although not by a whole lot)
    - Now produces the correct result for the sample input

--- day-19/day-19-part-2.py
+++ day-19/day-19-part-2.py
@@ -68,6 +68,32 @@ def add_workflow_from_line(workflows, line):
     workflows[workflow.id] = workflow    
     return
 
+def replace_workflow_id(workflows, old_workflow_id, new_workflow_id):
+    for workflow in workflows.values():
+        for rule in workflow.rules:
+            if rule.destination_workflow_id == old_workflow_id:
+                rule.destination_workflow_id = new_workflow_id
+
+def condense_workflows(workflows):
+    while True:
+        workflows_count = len(workflows)
+
+        workflow_ids_to_delete = set()
+        for workflow_id, workflow in workflows.items():
+            destination_workflow_ids = set()
+            for rule in workflow.rules:
+                destination_workflow_ids.add(rule.destination_workflow_id)
+            if len(destination_workflow_ids) == 1:
+                new_workflow_id = destination_workflow_ids.pop()
+                replace_workflow_id(workflows, workflow_id, new_workflow_id)
+                workflow_ids_to_delete.add(workflow_id)
+
+        for workflow_id in workflow_ids_to_delete:
+            del workflows[workflow_id]
+
+        if len(workflows) == workflows_count:
+            break
+
 def next_workflow_id(workflow, part):
     for rule in workflow.rules:
         next_workflow_id = rule.apply_to(part)
@@ -81,7 +107,9 @@ def is_accepted(part, workflows):
     return workflow_id == 'A'
 
 lines = read_input_file_lines()
+
 workflows = workflows_from_lines(lines)
+condense_workflows(workflows)
 
 x_values = set()
 m_values = set()
@@ -113,45 +141,44 @@ for workflow in workflows.values():
                 s_values.add(rule.compare_value - 1)
             else:
                 s_values.add(rule.compare_value + 1)
+
+# These need to be 0 and not 1 for the calculation to be correct for the lowest portion
+# of each range.
+x_values.add(0)
+m_values.add(0)
+a_values.add(0)
+s_values.add(0)
+x_values.add(4000)
+m_values.add(4000)
+a_values.add(4000)
+s_values.add(4000)
 x_values = sorted(x_values)
 m_values = sorted(m_values)
 a_values = sorted(a_values)
 s_values = sorted(s_values)
 
-# I guess I can do something like I was able to do yesterday:
-# - For each of x, m, a, s: 
-# - Create a list of values 0-4000 that, for each rule, just barely pass 
-#   each rule, and just barely fail it
-# - Run each combination of those values through the rules, and see which ones are accepted
-# - Calculate the size of each "range" 
-# - The total of those sizes is our answer
-
-print(x_values)
-
+print(f"x_values: {len(x_values)}")
 print(f'iterations: {len(x_values) * len(m_values) * len(a_values) * len(s_values)}')
 
-# TODO NEXT: Still way too many iterations. 
-# Maybe I can start with the rules with "R" results and work backwards and destroy rules from the 
-# list that lead only to "R" results? 
-# And/or the same with rules with "A" results?
-# Shorten up our list of rules, and therefore our search space?
+# TODO: Still too many iterations. 
 
 total_accepted_range_combinations = 0
-previous_x = 0
-previous_m = 0
-previous_a = 0
-previous_s = 0
-for x in x_values:
-    for m in m_values:
-        for a in a_values:
-            for s in s_values:
+for x_index in range(1, len(x_values)):
+    print (f"Processing x_index: {x_index}")
+    x = x_values[x_index]
+    previous_x = x_values[x_index - 1]
+    for m_index in range(1, len(m_values)):
+        m = m_values[m_index]
+        previous_m = m_values[m_index - 1]
+        for a_index in range(1, len(a_values)):
+            a = a_values[a_index]
+            previous_a = a_values[a_index - 1]
+            for s_index in range(1, len(s_values)):
+                s = s_values[s_index]
+                previous_s = s_values[s_index - 1]
                 machine_part = MachinePart(x, m, a, s)
                 if is_accepted(machine_part, workflows):
                     total_accepted_range_combinations += (x - previous_x) * (m - previous_m) * (a - previous_a) * (s - previous_s)
-                previous_s = s
-            previous_a = a
-        previous_m = m
-    previous_x = x
                     
 print(total_accepted_range_combinations)
 
